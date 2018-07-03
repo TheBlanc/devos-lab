0e639ebbbf0af0a2c063786e3be2d5822594173f
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2018-07-03 11:38:45 -0400

    In class solution

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Devo's Lab
 
 Let's write some tests for our `lab.rb` file.
+
+## Guidelines
+
+1. We want to test every method of our class at least once
+1. Test all possible return values
+1. We want to test the positive and negative cases
+1. Test updates to state (or attributes)
+1. Don't bother with private
--- lab_test.rb
+++ lab_test.rb
@@ -5,4 +5,58 @@ class LabTest < MiniTest::Test
   def setup
     @lab = Lab.new
   end
+
+  def test_less_than_zero_returns_true_when_less_than_zero
+    # arrange
+
+    # act
+    result = @lab.less_than_zero?(-1)
+
+    # assert
+    # assert result
+    assert_equal true, result
+  end
+
+  def test_less_than_zero_returns_false_when_not_less_than_zero
+    result = @lab.less_than_zero?(0)
+
+    assert_equal false, result
+  end
+
+  def test_less_than_zero_returns_false_when_positive
+    result = @lab.less_than_zero?(1)
+
+    assert_equal false, result
+  end
+
+  def test_is_zero_returns_true_when_is_zero
+    result = @lab.is_zero?(0)
+
+    assert_equal true, result
+  end
+
+  def test_is_zero_returns_false_when_positive
+    result = @lab.is_zero?(1)
+
+    assert_equal false, result
+  end
+
+  def test_is_zero_returns_false_when_negative
+    result = @lab.is_zero?(-1)
+
+    assert_equal false, result
+  end
+
+  def test_change_name_updates_name
+    @lab.change_name("Dexter's Lab")
+    result = @lab.name
+
+    assert_equal("Dexter's Lab", result)
+  end
+
+  def test_area_returns_area
+    result = @lab.area
+
+    assert_equal(200, result)
+  end
 end
